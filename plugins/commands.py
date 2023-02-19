84162795808859d0d4e36776174605913cb681f2
Vipzzk <96348929+Vipzzk@users.noreply.github.com>
2023-02-18 22:39:58 -0500

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -22,12 +22,12 @@ BATCH_FILES = {}
 async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         buttons = [[
-                    InlineKeyboardButton('➕ 𝖠𝖽𝖽 𝖬𝖾 𝖳𝗈 𝖸𝗈𝗎𝗋 𝖦𝗋𝗈𝗎𝗉 ➕', url=f"http://t.me/{temp.U_NAME}?startgroup=true")
+                    InlineKeyboardButton('👉 ꜱᴇᴀʀᴄʜ ᴍᴏᴠɪᴇ 👈', url=f"https://t.me/Pushpan_telegram")
                 ],[
                     InlineKeyboardButton('ℹ️ 𝖧𝖾𝗅𝗉', callback_data='help'),
                     InlineKeyboardButton('😊 𝖠𝖻𝗈𝗎𝗍', callback_data='about')
                 ],[
-                    InlineKeyboardButton('🍿 ᴏꜰꜰɪᴄɪᴀʟ ɢʀᴏᴜᴘ', url=f"https://t.me/RequestTP")
+                    InlineKeyboardButton('🍿 ᴛᴇʟᴇɢʀᴀᴍ ᴘᴜꜱʜᴘᴀɴ 🍿 ', url=f"https://t.me/Pushpan_telegram")
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup)
@@ -42,12 +42,12 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-                    InlineKeyboardButton('➕ 𝖠𝖽𝖽 𝖬𝖾 𝖳𝗈 𝖸𝗈𝗎𝗋 𝖦𝗋𝗈𝗎𝗉 ➕', url=f"http://t.me/{temp.U_NAME}?startgroup=true")
+                    InlineKeyboardButton('👉 ꜱᴇᴀʀᴄʜ ᴍᴏᴠɪᴇ 👈', url=f"https://t.me/Pushpan_telegram")
                 ],[
                     InlineKeyboardButton('ℹ️ 𝖧𝖾𝗅𝗉', callback_data='help'),
                     InlineKeyboardButton('😊 𝖠𝖻𝗈𝗎𝗍', callback_data='about')
                 ],[
-                    InlineKeyboardButton('🍿 ᴏꜰꜰɪᴄɪᴀʟ ɢʀᴏᴜᴘ', url=f"https://t.me/RequestTP")
+                    InlineKeyboardButton('🍿 ᴛᴇʟᴇɢʀᴀᴍ ᴘᴜꜱʜᴘᴀɴ 🍿 ', url=f"https://t.me/Pushpan_telegram")
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
@@ -87,12 +87,12 @@ async def start(client, message):
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-                    InlineKeyboardButton('➕ 𝖠𝖽𝖽 𝖬𝖾 𝖳𝗈 𝖸𝗈𝗎𝗋 𝖦𝗋𝗈𝗎𝗉 ➕', url=f"http://t.me/{temp.U_NAME}?startgroup=true")
+                    InlineKeyboardButton('👉 ꜱᴇᴀʀᴄʜ ᴍᴏᴠɪᴇ 👈', url=f"https://t.me/Pushpan_telegram")
                 ],[
                     InlineKeyboardButton('ℹ️ 𝖧𝖾𝗅𝗉', callback_data='help'),
                     InlineKeyboardButton('😊 𝖠𝖻𝗈𝗎𝗍', callback_data='about')
                 ],[
-                    InlineKeyboardButton('🍿 ᴏꜰꜰɪᴄɪᴀʟ ɢʀᴏᴜᴘ', url=f"https://t.me/RequestTP")
+                    InlineKeyboardButton('🍿 ᴛᴇʟᴇɢʀᴀᴍ ᴘᴜꜱʜᴘᴀɴ 🍿 ', url=f"https://t.me/Pushpan_telegram")
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
